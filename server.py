684c1eca432db513798d1287755a666a0be9d801
ckushner <colekushner@gmail.com>
2014-01-25 11:01:36 -0800

    testing send

--- server.py
+++ server.py
@@ -19,6 +19,15 @@ VID_EXTENSION = ['mp4']
 def begin():
 	return "don't be lazy man!"
 
+#upload img/vid file for sending 
+@app.route("/upload")
+def upload():
+	#save file on server
+	snap = request.files['file']
+	filename = secure_filename(snap.filename)
+	snap.save(os.path.join(app.config['SEND_UPLOAD_FOLDER'], filename))
+	return os.path.join(app.config['SEND_UPLOAD_FOLDER'], filename)
+
 #send image or video
 #json reqs: {'username', 'password', 'file', 'recipient'}
 @app.route("/send/<filetype>", methods=['POST'])
@@ -28,34 +37,16 @@ def send(filetype):
 	s = Snapchat()
 	s.login(data['username'],data['password'])
 
-	return "success!"
-
-	"""
-	#check that file has a correct ext
-	filename, ext = name.split('.')
-	if (filetype == "image") and (ext not in IMG_EXTENSIONS):
-		print("invalid image format")
-		continue
-	if (filetype == "video") and (ext not in VID_EXTENSIONS):
-		print("invalid video format")
-		continue
-	"""
-
-	#save file on server
-	snap = request.files['file']
-	filename = secure_filename(snap.filename)
-	snap.save(os.path.join(app.config['SEND_UPLOAD_FOLDER'], filename))
-	
 	#upload file to snapchat
 	if (filetype == "image"):
 		snapformat = "Snapchat.MEDIA_IMAGE"
 	if (filetype == "video"):
 		snapformat = "Snapchat.MEDIA_VIDEO"
-	media_id = s.upload(snapformat, send_from_directory(app.config['SEND_UPLOAD_FOLDER'],filename))
+	media_id = s.upload(snapformat, data['fileloc'])
 	
 	#send to recipient
 	s.send(media_id, data['recipient'])
-	#return "success!"
+	return "success!"
 
 #getall
 @app.route("/getall")
