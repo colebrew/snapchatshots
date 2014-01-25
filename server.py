d9fad4b384ab43637d03401e3f71a1190b38516e
ckushner <colekushner@gmail.com>
2014-01-25 09:48:12 -0800

    testing send

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def begin():
 
 #send image or video
 #json reqs: {'username', 'password', 'file', 'recipient'}
-@app.route("/send", methods=['POST'])
+@app.route("/send/<filetype>", methods=['POST'])
 def send(filetype):
 	#login 
 	data = request.get_json()
