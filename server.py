4b3ffd4bc853d58baec5ca8f2f17872ba7cc3440
ckushner <colekushner@gmail.com>
2014-01-25 11:21:16 -0800

    testing send

--- server.py
+++ server.py
@@ -38,7 +38,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['SEND_UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
+            return redirect(url_for('upload_file',
                                     filename=filename))
     return 'oops'
 
