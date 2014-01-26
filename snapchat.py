e6098d39872fb19c2344fbce1b230e9f2e0443a8
Austin Feight <afeight@umich.edu>
2014-01-26 08:36:55 -0800

    pushing update

--- snapchat.py
+++ snapchat.py
@@ -164,10 +164,9 @@ class Snapchat:
 
         # If the status code isn't 200, it's a failed request.
         if r.status_code != 200:
-            if False:
-                print 'Post returned code: ', r.status_code, 'for request', endpoint, data
-                print 'Error content:'
-                print r.content
+            print 'Post returned code: ', r.status_code, 'for request', endpoint, data
+            print 'Error content:'
+            print r.content
             return False
 
         # If possible, try to return a json object.
